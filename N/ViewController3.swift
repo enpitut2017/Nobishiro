14546513529e8aa09f2cb242c6ab68ebb99bf45f
Omuro Takaho <tuckahoe0107@gmail.com>
2017-08-07 15:41:45 +0900

    「眠りにつけ」の画面に切り替わるタイミングを調整

--- N/ViewController3.swift
+++ N/ViewController3.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController3: UIViewController {
     
     var timer:Timer = Timer()
+    var diffMinute: Int = 0
+    var diffHour: Int = 0
+    let strHour:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+    let strMinute:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        timer = Timer.scheduledTimer(timeInterval: 5,
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd-HH-mm-ss"
+        let now = Date()
+
+        print(formatter.string(from: now))
+        let str = formatter.string(from: now)
+        let split = str.components(separatedBy: "-")
+        
+        let diff = timeDiff(hour: Int(split[3])!,minute: Int(split[4])!,second: Int(split[5])!)
+        print("diff")
+        print(diff)
+        
+        timer = Timer.scheduledTimer(timeInterval: TimeInterval(diff),
                                      target: self,                   //
             selector: #selector(ViewController3.changeView),
             userInfo: nil,                  //
@@ -24,6 +43,30 @@ class ViewController3: UIViewController {
     
     func changeView() {
         self.performSegue(withIdentifier: "toSleep",sender: nil)
+    }
+    
+    func timeDiff(hour: Int, minute: Int, second:Int)-> Int {
+        if Int(strHour.Hour)! - hour < 0 {
+            diffHour = -1*(Int(strHour.Hour)!-hour)+24
+            print(diffHour)
+            print("a")
+        }
+        else {
+            diffHour = Int(strHour.Hour)!-hour
+            print(diffHour)
+            print("b")
+        }
+        if Int(strMinute.Minute)! - minute < 0 {
+            diffMinute = -1*(Int(strMinute.Minute)!-minute)+59
+            print(diffMinute)
+            print("c")
+        }
+        else {
+            diffMinute = Int(strMinute.Minute)!-minute-1
+            print(diffMinute)
+            print("d")
+        }
+        return 3600*diffHour+60*diffMinute+60-second
     }
     
     
